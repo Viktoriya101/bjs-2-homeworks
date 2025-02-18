a5b3d4ef6f908f94c6b0de543e88bb2755a1d121
Viktoriya <vika.belinskaya1995@gmail.com>
2025-02-18 20:44:44 +0500

    [200~1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,3 +8,40 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
+
+"use strict";
+
+function solveEquation(a, b, c) {
+    let arr = [];
+    
+    const discriminant = Math.pow(b, 2) - 4 * a * c;
+    
+    if (discriminant < 0) {
+        return arr;
+    } else if (discriminant === 0) {
+        const root = -b / (2 * a);
+        arr.push(root);
+    } else {
+        const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        arr.push(root1, root2);
+    }
+
+    return arr;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+    let monthlyRate = percent / 100 / 12;
+    
+    let loanAmount = amount - contribution;
+
+    if (loanAmount <= 0) {
+        return 0;
+    }
+
+    let monthlyPayment = loanAmount * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+    
+    let totalAmount = monthlyPayment * countMonths;
+
+    return parseFloat(totalAmount.toFixed(2));
+}
