bc14c643fa354c2f1c92700173cf2750032ca900
Viktoriya <vika.belinskaya1995@gmail.com>
2025-02-26 20:02:15 +0500

    task.js after check

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,12 +5,10 @@ function solveEquation(a, b, c) {
     
     const discriminant = Math.pow(b, 2) - 4 * a * c;
     
-    if (discriminant < 0) {
-        return arr;
-    } else if (discriminant === 0) {
+    if (discriminant === 0) {
         const root = -b / (2 * a);
         arr.push(root);
-    } else {
+    } else if (discriminant > 0) {
         const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
         const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
         arr.push(root1, root2);
