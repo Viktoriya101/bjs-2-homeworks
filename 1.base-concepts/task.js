36c05ae3353a1eae40d552d9bdd1abe0109997c0
Viktoriya Belinskaya <vika.belinskaya1995@gmail.com>
2026-02-26 11:43:42 +0500

    changed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let d = Math.pow(b, 2) - 4 * a * c;
   let arr = [];
-  
+  if(d == 0){
+    let r = -b / (2 * a);
+    arr.push(r);
+  } else if(d > 0) {
+    let r1 = (-b + Math.sqrt(d)) / (2 * a);
+    let r2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(r1, r2);
+  } 
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthPercent = percent / 100 / 12;
+  let bodyLoan = amount - contribution;
+  let payment = bodyLoan * (monthPercent + (monthPercent / (Math.pow((1 + monthPercent), countMonths) - 1)));
+  let totalAmount = Number((payment * countMonths).toFixed(2));
+return totalAmount;
 }
