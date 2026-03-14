62f46f3e99c9374957825452d1073c735e8e53eb
Viktoriya Belinskaya <vika.belinskaya1995@gmail.com>
2026-03-14 22:39:11 +0500

    changed task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿//Задача 1. Форматтер чисел
+function parseCount(value) {
+	let pars = Number.parseFloat(value);
+	if(isNaN(pars)) {
+		throw new Error("Невалидное значение");
+	}
+	return pars;
+}
+
+function validateCount(value) {
+	try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+//Задача 2. Треугольник 
+class Triangle {
+	constructor(a, b, c) {
+		if((a >= b + c) || (b >= a + c) || (c >= a + b)) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		let p = this.perimeter / 2;
+    	return Number.parseFloat((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try { 
+    	return new Triangle(a, b, c);
+  } catch {
+    	return { 
+      		get perimeter() { 
+        		return "Ошибка! Треугольник не существует"; 
+      		},
+      		get area() { 
+        		return "Ошибка! Треугольник не существует"; 
+      		}
+    	}
+  	}
+}
